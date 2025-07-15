aedc4430c5668c7f7bf34b22b57dd8e95d6aa219
Ronit Mexson <ronitmexson9@gmail.com>
2025-07-15 08:16:10 +0530

    Update MyClass.java

--- Day 1/MyClass.java
+++ Day 1/MyClass.java
@@ -17,3 +17,8 @@ public class MyClass {
     
   }
 }
+/*
+  Today I learnt a lot of things related to Java i discovered a lot of things along this way 
+  got to know a lot of yhings which were new to me like the difference between print and println
+  I will upload my daily progress in this repo.
+*/
